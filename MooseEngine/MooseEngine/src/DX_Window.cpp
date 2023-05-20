2ee84db265cb03bff5a7e740136d8077e05165bc
adamdeere <adam.deere@hotmail.com>
2023-05-20 16:07:37 +0100

    set up for a DX window done

---
+++ MooseEngine/MooseEngine/src/DX_Window.cpp
@@ -0,0 +1,23 @@
+#include "DX_Window.h"
+
+DX_Window::DX_Window()
+{
+}
+
+DX_Window::~DX_Window()
+{
+}
+
+HRESULT DX_Window::InitWindow(const HINSTANCE hInstance, const int nCmdShow)
+{
+    return E_NOTIMPL;
+}
+
+HRESULT DX_Window::InitDevice()
+{
+    return E_NOTIMPL;
+}
+
+void DX_Window::Run()
+{
+}
